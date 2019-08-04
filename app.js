60f00d90ccf902d9bb7fb9690d03b408cc44ea12
t-leith <leithtorrens@gmail.com>
2019-08-04 20:15:16 +1200

    bla

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("hello")
+// $("#test_button").hide()
+// $("#test_button").slideDown(5000)
+
+$("#test_button").click(function(){
+    $("#test").append("<h1>fartaloft</h1>")
+    $("#test_button").slideUp(5000)
+    
+})
